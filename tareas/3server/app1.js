580de375f44a0c1d317b2a87589925052891800f
Elias14AR <119818156+Elias14AR@users.noreply.github.com>
2025-05-06 17:22:56 -0600

    Add files via upload

---
+++ tareas/3server/app1.js
@@ -0,0 +1,18 @@
+// Importar express
+const express = require('express');
+
+// Crear una instancia
+const app = express();
+
+// Definir el puerto
+const port = 1230;
+
+// Configuración de ruta GET para la raíz ('/')
+app.get('/', (req, res) => {
+    res.send('Hola mundo desde el puerto ' + port);
+});
+
+// Iniciar el servidor y escuchar en el puerto
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://127.0.0.1:${port}`);
+});
